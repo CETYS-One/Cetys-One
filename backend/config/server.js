364fb0f999b15a5791c25f4d2e4bf4f24e73649e
Mau MD <j.mauricio.munoz1@gmail.com>
2022-03-30 09:22:18 -0700

    agregando servidor

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e29ac8a858aecf52ea2fad7e45405063'),
+    },
+  },
+});
