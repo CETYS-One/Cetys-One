1092688eb15c15840edb079df3db2b28c1cc2c8d
Mau MD <j.mauricio.munoz1@gmail.com>
2022-04-16 10:52:40 -0700

    updated font

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,94 @@
 import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider } from "native-base";
+import { extendTheme, NativeBaseProvider } from "native-base";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Pages from "./screens/Pages";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
+import {
+  useFonts,
+  Montserrat_100Thin,
+  Montserrat_100Thin_Italic,
+  Montserrat_200ExtraLight,
+  Montserrat_200ExtraLight_Italic,
+  Montserrat_300Light,
+  Montserrat_300Light_Italic,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_500Medium,
+  Montserrat_500Medium_Italic,
+  Montserrat_600SemiBold,
+  Montserrat_600SemiBold_Italic,
+  Montserrat_700Bold,
+  Montserrat_700Bold_Italic,
+} from "@expo-google-fonts/montserrat";
+
+const nativeTheme = extendTheme({
+  fontConfig: {
+    Montserrat: {
+      100: {
+        normal: "Montserrat_100Thin",
+        italic: "Montserrat_100Thin_Italic",
+      },
+      200: {
+        normal: "Montserrat_200ExtraLight",
+        italic: "Montserrat_200ExtraLight_Italic",
+      },
+      300: {
+        normal: "Montserrat_300Light",
+        italic: "Montserrat-300Light_Italic",
+      },
+      400: {
+        normal: "Montserrat_400Regular",
+        italic: "Montserrat_400Regular_Italic",
+      },
+      500: {
+        normal: "Montserrat_500Medium",
+        italic: "Montserrat_500Medium_Italic",
+      },
+      600: {
+        normal: "Montserrat_600SemiBold",
+        italic: "Montserrat_600SemiBold_Italic",
+      },
+      700: {
+        normal: "Montserrat_700Bold",
+        italic: "Montserrat_700Bold_Italic",
+      },
+    },
+  },
+  fonts: {
+    heading: "Montserrat",
+    body: "Montserrat",
+    mono: "Montserrat",
+  },
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_100Thin_Italic,
+    Montserrat_200ExtraLight,
+    Montserrat_200ExtraLight_Italic,
+    Montserrat_300Light,
+    Montserrat_300Light_Italic,
+    Montserrat_400Regular,
+    Montserrat_400Regular_Italic,
+    Montserrat_500Medium,
+    Montserrat_500Medium_Italic,
+    Montserrat_600SemiBold,
+    Montserrat_600SemiBold_Italic,
+    Montserrat_700Bold,
+    Montserrat_700Bold_Italic,
+  });
 
-  if (!isLoadingComplete) {
+  if (!isLoadingComplete || !fontsLoaded) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <NativeBaseProvider>
+        <NativeBaseProvider theme={nativeTheme}>
           <Pages />
           <StatusBar />
         </NativeBaseProvider>
--- frontend/components/Product/Cantidad.tsx
+++ frontend/components/Product/Cantidad.tsx
@@ -1,35 +1,65 @@
-import {Box, Text, Image, Flex, Center, VStack, HStack, TextArea, Button} from "native-base";
-import {useState} from 'react';
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  Center,
+  VStack,
+  HStack,
+  TextArea,
+  Button,
+} from "native-base";
+import { useState } from "react";
 
 const Cantidad = () => {
-
   const [quantity, setQuantity] = useState(1);
 
-  function addOne(){
+  function addOne() {
     setQuantity(quantity + 1);
   }
 
-  function reduceOne(){
-    if(quantity > 1){
+  function reduceOne() {
+    if (quantity > 1) {
       setQuantity(quantity - 1);
     }
   }
 
-  return(
-          <VStack alignItems={"center"} space={2}>
-            <Text fontSize={"20px"}>Cantidad</Text>
-            <HStack justifyContent={"center"} alignItems={"center"} space={3}>
-              <Button bgColor={"white"} color={"black"} borderRadius={"10000"} h={"40px"} w={"40px"} onPress={reduceOne} borderWidth={"1px"} borderColor={"gray.300"}>
-                <Text fontWeight={"bold"} fontSize={"17px"}>-</Text>
-              </Button>
-              <Text>{quantity}</Text>
-              <Button bgColor={"white"} color={"black"} borderRadius={"10000"} h={"40px"} w={"40px"} borderWidth={"1px"} borderColor={"gray.300"} onPress={addOne}>
-                <Text fontWeight={"bold"} fontSize={"17px"}>+</Text>
-              </Button>           
-            </HStack>
-          </VStack>
-
+  return (
+    <VStack alignItems={"center"} space={2}>
+      <Text fontSize={"20px"}>Cantidad</Text>
+      <HStack justifyContent={"center"} alignItems={"center"} space={3}>
+        <Button
+          bgColor={"white"}
+          color={"black"}
+          borderRadius={"10000"}
+          h={"40px"}
+          w={"40px"}
+          onPress={reduceOne}
+          borderWidth={"1px"}
+          borderColor={"gray.300"}
+        >
+          <Text fontWeight={"bold"} adjustsFontSizeToFit>
+            -
+          </Text>
+        </Button>
+        <Text>{quantity}</Text>
+        <Button
+          bgColor={"white"}
+          color={"black"}
+          borderRadius={"10000"}
+          h={"40px"}
+          w={"40px"}
+          borderWidth={"1px"}
+          borderColor={"gray.300"}
+          onPress={addOne}
+        >
+          <Text fontWeight={"bold"} adjustsFontSizeToFit textAlign={"center"}>
+            +
+          </Text>
+        </Button>
+      </HStack>
+    </VStack>
   );
-}
+};
 
 export default Cantidad;
