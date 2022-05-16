7283423e7de82dcd20a2a310853d6afaec6334aa
Daniel Barocio <danybarociov@gmail.com>
2022-05-16 15:44:34 -0700

    estadisticas

--- frontend/screens/seller/Statistics.tsx
+++ frontend/screens/seller/Statistics.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "react-query";
 import { useAuth } from "../../hooks/useAuth";
 import { useAxios } from "../../hooks/useAxios";
 import { IOrder } from "../../types/strapi";
+import { interpolateNode } from "react-native-reanimated";
 
 const Statistics = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -16,12 +17,68 @@ const Statistics = () => {
   const axios = useAxios(user?.jwt);
 
   const { data: statistics } = useQuery("statistics", async () => {
-    const res = await axios.get<IOrder>("/orders/statistics/me");
-
+    const res = await axios.get<IOrder[]>("/orders/statistics/me");
+    // lo que hace esta wea es nomas retornar todas las ordenes de la cafeteria.
+    // te toca a ti filtrarlo primero por dia, y de ahi obtener todo lo que hace falta
     const orders = res.data;
     // Debes de obtener la cantidad de productos vendidos, el producto mas vendido y el ingreso obtenido DE HOY.
 
-    return orders;
+    let util: {
+      num: number;
+      product: string;
+      profit: number;
+    } = { num: 0, product: "", profit: 0 };
+
+    const todayOrders = orders.filter((element) => {
+      let todayDate = new Date();
+      let day1 = element.createdAt.split("T")[0];
+      let day2 = todayDate.toISOString().split("T")[0];
+      console.log(day1, day2);
+      return day1 == day2;
+    });
+
+    const result = todayOrders.reduce((anterior: number, actual) => {
+      anterior += actual.items.reduce((before: number, now) => {
+        before += now.quantity * now.product.price;
+        return before;
+      }, 0);
+      return anterior;
+    }, 0);
+
+    let productsName: {
+      [key: string]: number;
+    } = {};
+
+    todayOrders.forEach((order) => {
+      order.items.forEach((item) => {
+        console.log([productsName[item.product.name]]);
+        if (productsName[item.product.name]) {
+          console.log("si jala");
+          productsName[item.product.name]++;
+        } else {
+          productsName[item.product.name] = 1;
+        }
+      });
+    });
+
+    let array = Object.keys(productsName);
+    let max = 0;
+    let name = "";
+    for (let i = 0; i < array.length; i++) {
+      if (productsName[array[i]] > max) {
+        max = productsName[array[i]];
+        name = array[i];
+      }
+    }
+    console.log(productsName);
+    console.log(max);
+    console.log(name);
+
+    util["num"] = todayOrders.length;
+    util["profit"] = result;
+    util["product"] = name;
+
+    return util;
   });
 
   return (
@@ -30,19 +87,19 @@ const Statistics = () => {
         <Box>
           <Text fontSize={18}>Productos vendidos</Text>
           <Text fontWeight={"600"} fontSize={30}>
-            53
+            {statistics && statistics.num}
           </Text>
         </Box>
         <Box>
           <Text fontSize={18}>Producto mas vendido hoy</Text>
           <Text fontWeight={"600"} fontSize={30}>
-            Hamburguesa de Pollo
+            {statistics && statistics.product}
           </Text>
         </Box>
         <Box>
           <Text fontSize={18}>Ingreso obtenido</Text>
           <Text fontWeight={"600"} fontSize={30}>
-            1050.20
+            ${statistics && statistics.profit}
           </Text>
         </Box>
       </VStack>
